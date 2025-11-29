0a320b3bb1488e7624fc5dff4557c631b7d690bb
Shin Sungkyu <106169271+ssk1109@users.noreply.github.com>
2025-11-29 11:35:16 +0000

    2025.11.29 api comprehenstion

--- military-inflearn-chatgpt/ch01/env_test.py
+++ military-inflearn-chatgpt/ch01/env_test.py
@@ -6,3 +6,11 @@ matplotlib.use("Agg")
 plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
 plt.savefig("plot.png")
+
+# 가상환경 생성: python -m venv 가상환경명
+# 가상환경 실행: 윈도우: 가상환경명\Script\activate.bat
+#               리눅스/맥: source 가상환경명/bin/activate
+
+# vs 내의 python script를 venv와 연결: view\command palette\python select interpreter 검색/설치한 가상환경 선택 or 가상환경 경로 입력
+
+# codespace는 리눅스 + gui 부재로 matplotlib를 정상적으로 실행할 수 없기에 결과를 이미지로 저장하여 확인
